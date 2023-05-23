9cce6d53c9053960f78d24d86108828cffbd45d7
DESKTOP-TNP0ACF\benoi <benoit.bousseronde@epita.fr>
2023-05-23 09:44:09 +0200

    test1

---
+++ src/main/java/org/example/ERPHumeur/ErpHumeurApplication.java
@@ -0,0 +1,13 @@
+package org.example.ERPHumeur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpHumeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpHumeurApplication.class, args);
+	}
+
+}
